c174d5d855672da4881bb9b78051509543193d0e
felixvl <digitaleservice@gmail.com>
2024-09-19 14:49:58 +0200

    werkend id

--- allefuncties.py
+++ allefuncties.py
@@ -36,3 +36,23 @@ def artikeltoevoegen_af(titel, auteur, categorie):
 
     mydb.commit()
     return "opgeslagen"
+
+def artikelmbvid_af(artikelid):
+    mydb = mysql.connector.connect(
+    host="aidatabasedontdelete.mysql.database.azure.com",  #port erbij indien mac
+    user="felixadmin",
+    password=os.environ.get('ONZEDATABASEWACHTWOORD'),
+    database="krantenapp"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM artikel WHERE id="+artikelid)
+
+    myresult = mycursor.fetchall()
+    keys = [i[0] for i in mycursor.description]
+
+    data = [
+        dict(zip(keys, row)) for row in myresult
+    ]
+    return data
